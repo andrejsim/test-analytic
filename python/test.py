6e4f7a17ce93b00e5ecd3b6bfc0cde6700c97ee7
andrejsim <andrej.mihajlovski@meteogroup.com>
2019-03-22 15:27:05 +0100

    search over grib file for adequet metadata. (pass on to peter)

--- python/test.py
+++ python/test.py
@@ -1,10 +1,26 @@
 import cfgrib
 import matplotlib.pyplot as plt
-
+from pprint import pprint
 
 ds = cfgrib.open_dataset('../data/20190204T070000Z_TT.grb')
 d = ds.t.isel()
-print(d)
+
+pprint(ds.to_dict(data=False))
+
+print(dict(ds.variables.mapping).keys())
+print(ds.time.to_dict())
+print(ds.time.data)
+print(ds.step.data)
+print(dict(ds.coords.variables.mapping).keys())
+
+#print(ds.coords.variables)
+
+#print(ds.data_vars.variables.mapping)
+
+#print(ds)
+#print('-----')
+#print(d)
+
 d.plot()
 plt.show()
 
