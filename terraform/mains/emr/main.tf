9d1e59647cb733a03ca93a97116e2bf5b2290b13
andrejsim <andrej.mihajlovski@meteogroup.com>
2019-05-09 15:24:52 +0200

    terraform deploy of amazon emr instance to datafabric account.

--- terraform/mains/emr/main.tf
+++ terraform/mains/emr/main.tf
@@ -1,43 +1,198 @@
-resource "aws_dynamodb_table" "geospatial-dynamodb-table" {
-  name           = "geospatial-store"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 20
-  write_capacity = 20
-  hash_key       = "geojson"
-  range_key      = "datapoint"
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
 
-  attribute {
-    name = "geojson"
-    type = "S"
-  }
-
-  attribute {
-    name = "datapoint"
-    type = "S"
-  }
-
-  attribute {
-    name = "time"
-    type = "N"
-  }
-
-  ttl {
-    attribute_name = "timetolive"
-    enabled        = false
-  }
-
-  global_secondary_index {
-    name               = "datapoint-index"
-    hash_key           = "datapoint"
-    range_key          = "time"
-    write_capacity     = 10
-    read_capacity      = 10
-    projection_type    = "INCLUDE"
-    non_key_attributes = ["geojson"]
-  }
+resource "aws_subnet" "main" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.1.0/24"
 
   tags = {
-    Name        = "dynamodb-table-geospatial-store"
-    Environment = "temporarytesting"
+    Name = "Main"
   }
+}
+
+resource "aws_security_group" "sg" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    # TLS (change to whatever ports you need)
+    from_port   = 443
+    to_port     = 443
+    protocol    = "-1"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = "10.0.0.0/16" # add a CIDR block here
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = ["pl-12c4e678"]
+  }
+}
+
+resource "aws_iam_role" "iam_emr_service_role" {
+  name = "datafabric_iam_emr_service_role"
+
+  assume_role_policy = <<EOF
+{
+  "Action": "iam:PassRole",
+  "Effect": "Allow",
+  "Resource": [
+    "arn:aws:iam::*:role/EMR_DefaultRole",
+    "arn:aws:iam::*:role/EMR_EC2_DefaultRole",
+    "arn:aws:iam::*:role/EMR_AutoScaling_DefaultRole"
+  ]
+}
+EOF
+
+  tags = {
+    created = "terraform"
+    user    = "datafabric"
+  }
+}
+
+resource "aws_iam_instance_profile" "emr_profile" {
+  name = "test_profile"
+  role = "${aws_iam_role.role.name}"
+}
+
+resource "aws_iam_role" "role" {
+  name = "test_role"
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "Service": "ec2.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_emr_cluster" "cluster" {
+  name          = "emr-test-arn"
+  release_label = "emr-4.6.0"
+  applications  = ["Spark"]
+  additional_info = <<EOF
+{
+  "instanceAwsClientConfiguration": {
+    "proxyPort": 8099,
+    "proxyHost": "myproxy.example.com"
+  }
+}
+EOF
+
+  termination_protection            = false
+  keep_job_flow_alive_when_no_steps = true
+
+  ec2_attributes {
+    subnet_id                         = "${aws_subnet.main.id}"
+    emr_managed_master_security_group = "${aws_security_group.sg.id}"
+    emr_managed_slave_security_group  = "${aws_security_group.sg.id}"
+    instance_profile                  = "${aws_iam_instance_profile.emr_profile.arn}"
+  }
+
+  instance_group {
+    instance_role  = "CORE"
+    instance_type  = "c4.large"
+    instance_count = "1"
+    ebs_config {
+      size                 = "40"
+      type                 = "gp2"
+      volumes_per_instance = 1
+    }
+    bid_price          = "0.30"
+    autoscaling_policy = <<EOF
+{
+"Constraints": {
+  "MinCapacity": 1,
+  "MaxCapacity": 2
+},
+"Rules": [
+  {
+    "Name": "ScaleOutMemoryPercentage",
+    "Description": "Scale out if YARNMemoryAvailablePercentage is less than 15",
+    "Action": {
+      "SimpleScalingPolicyConfiguration": {
+        "AdjustmentType": "CHANGE_IN_CAPACITY",
+        "ScalingAdjustment": 1,
+        "CoolDown": 300
+      }
+    },
+    "Trigger": {
+      "CloudWatchAlarmDefinition": {
+        "ComparisonOperator": "LESS_THAN",
+        "EvaluationPeriods": 1,
+        "MetricName": "YARNMemoryAvailablePercentage",
+        "Namespace": "AWS/ElasticMapReduce",
+        "Period": 300,
+        "Statistic": "AVERAGE",
+        "Threshold": 15.0,
+        "Unit": "PERCENT"
+      }
+    }
+  }
+]
+}
+EOF
+  }
+  ebs_root_volume_size = 100
+
+  master_instance_type = "m5.xlarge"
+  core_instance_type   = "m5.xlarge"
+  core_instance_count  = 1
+
+  tags = {
+    role = "rolename"
+    env  = "env"
+  }
+
+  bootstrap_action {
+    path = "s3://elasticmapreduce/bootstrap-actions/run-if"
+    name = "runif"
+    args = ["instance.isMaster=true", "echo running on master node"]
+  }
+
+  configurations_json = <<EOF
+  [
+    {
+      "Classification": "hadoop-env",
+      "Configurations": [
+        {
+          "Classification": "export",
+          "Properties": {
+            "JAVA_HOME": "/usr/lib/jvm/java-1.8.0"
+          }
+        }
+      ],
+      "Properties": {}
+    },
+    {
+      "Classification": "spark-env",
+      "Configurations": [
+        {
+          "Classification": "export",
+          "Properties": {
+            "JAVA_HOME": "/usr/lib/jvm/java-1.8.0"
+          }
+        }
+      ],
+      "Properties": {}
+    }
+  ]
+EOF
+  service_role        = "${aws_iam_role.iam_emr_service_role.arn}"
 }
